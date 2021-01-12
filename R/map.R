4151aa2d5420fc06e9434251d8ba7b38bdc2e677
JohnCoene <jcoenep@gmail.com>
2021-01-12 14:21:01 +0100

    init map-related functions

--- R/map.R
+++ R/map.R
@@ -1,24 +1,132 @@
 #' Get Map
 #' 
-#' Retrieve map data from [echarts4r.maps](https://github.com/JohnCoene/echarts4r.maps).
+#' Retrieve map data to pass to the `maps` 
+#' argument of the `fig_map()` function.
 #' 
-#' @param name The name of the map.
+#' @param iso3c Iso3c code of the country to retrieve, e.g.: `USA`.
+#' @param keep Proportion of points to retain, it is highly recommended
+#' to reduce the detail of the map or it will take too long to load in the
+#' browser. Set to `NULL` to not reduce the amount of details.
+#' @param name Name of the database to use.
+#' @param region Character vector that names the polygons to draw.
+#' @param level Level of the polygons to draw, either the region or
+#' subregion.
+#' 
+#' @section Functions:
+#' 
+#' - `get_gadm_data`: Retrieves country-level data from [gadm.org](https://gadm.org/).
+#' - `get_map_data`: Uses the `maps::map()` function to retrieve the map data, similar to `ggplot2::map_data`.
+#' - `get_world_map`: Retrives a world map (from GeoJSON).
 #' 
 #' @importFrom jsonlite read_json
+#' @importFrom tibble tibble
+#' @importFrom purrr map_dfr
+#' @importFrom utils download.file
+#' 
+#' @name map_data
 #' @export 
-get_map_data <- function(name = "World"){
+get_gadm_data <- function(iso3c, level = c(0, 1, 2, 3, 4), keep = 0.05){
+
+  check_package("rmapshaper")
+  check_package("geojsonio")
+
+  if(missing(iso3c))
+    stop("Missing `iso3c`", call. = FALSE)
+
+  # ensure it's all caps
+  iso3c <- toupper(iso3c)
+
+  # base GADM URL
+  BASE <- "https://biogeo.ucdavis.edu/data/gadm3.6/Rsp/gadm36_%s_%s_sp.rds"
+
+  # download to temp
+  url <- sprintf(BASE, iso3c, level)
+  temp <- tempfile(fileext = ".rds")
+  on.exit({
+    unlink(temp)
+  })
+  dl <- tryCatch(
+    download.file(url, temp, quiet = TRUE),
+    error = function(e) e
+  )
+
+  if(inherits(dl, "error"))
+    stop("Failed to download data, is the `iso3c` code correct?", call. = FALSE)
+
+  sp <- readRDS(temp)
+
+  if(!is.null(keep))
+    sp <- suppressWarnings(
+      rmapshaper::ms_simplify(sp, keep = keep)
+    )
+
+  sp
+}
+
+#' @rdname map_data
+#' @export 
+get_map_data <- function(
+  region = ".",
+  level = c("region", "subregion"),
+  name = "world"
+){
+  check_package("maps")
+
+  level <- match.arg(level)
+
+  map <- maps::map(
+    name, 
+    region, 
+    exact = FALSE, 
+    plot = FALSE, 
+    fill = TRUE
+  )
+
+  df <- tibble(
+    longitude = map$x,
+    latitude = map$y,
+    group = cumsum(is.na(map$x) & is.na(map$y)) + 1,
+    order = seq_along(map$x)
+  )
+
+  names <- do.call("rbind", lapply(strsplit(map$names, "[:,]"), "[", 1:2))
+  df$region <- names[df$group, 1]
+  df$subregion <- names[df$group, 2]
+  df <- df[stats::complete.cases(df$latitude, df$longitude), ]
+
+  split(df, df[[level]]) %>% 
+    map_dfr(function(grp, lvl){
+      t <- tibble(
+        longitude = list(grp$longitude), 
+        latitude = list(grp$latitude)
+      )
+      
+      t[["level"]] <- unique(grp[[lvl]])
+      return(t)
+    }, lvl = level)
+}
+
+#' @rdname map_data
+#' @export 
+get_world_map <- function(){
 
   check_alter()
   
-  BASE <- "https://raw.githubusercontent.com/JohnCoene/echarts4r.maps/master/inst/%s.json"
+  url <- "https://raw.githubusercontent.com/JohnCoene/echarts4r.maps/master/inst/World.json"
 
-  url <- sprintf(BASE, name)
   json <- read_json(url)
 
   map_alter(json)
 }
 
+#' Alter GeoJSON
+#' 
+#' @param geojson Geojson as list.
+#' 
+#' @keywords internal
 map_alter <- function(geojson){
+  check_alter()
+  
   dataset <- alter::Alter$new(geojson)$
     source(type = "GeoJSON")$
     get("rows")
@@ -31,16 +139,25 @@ map_alter <- function(geojson){
 #' Add a map figure.
 #' 
 #' @inheritParams fig_point
-#' @param map Name of map, or map object as returned by 
-#' [get_map_data()], or a `SpatialPolygonsDataFrame` as
-#' as returned by [raster::getData], or a `geo_list` as
+#' @param map Name of map to pass to the `region` argument
+#' of the [get_map_data()] function, or map object as returned by 
+#' [get_world_map()], [get_gadm_data()], or [get_map_data()],
+#' or a `SpatialPolygonsDataFrame` as
+#' returned by [raster::getData()], or a `geo_list` as
 #' obtained from `geojsonio::geojson_list()`.
+#' 
+#' @examples 
+#' g2() %>% 
+#'  fig_map(stroke = "#fff", fill = "gray") %>% 
+#'  axis_hide()
 #' 
 #' @export 
 fig_map <- function(
   g, 
   ...,
-  map = "World"
+  inherit_asp = TRUE,
+  sync = TRUE,
+  map = get_world_map()
 ){
   UseMethod("fig_map")
 }
@@ -50,11 +167,12 @@ fig_map <- function(
 fig_map.g2r <- function(
   g,
   ...,
-  map = "World",
-  keep = 0.05
+  inherit_asp = TRUE,
+  sync = TRUE,
+  map = get_world_map()
 ) {
 
-  data <- map_data(map, keep = 0.05)
+  data <- map_data(map)
 
   asp <- get_combined_asp(g, ..., inherit_asp = inherit_asp)
   asp$x <- "longitude"
@@ -76,7 +194,13 @@ map_data <- function(map, ...) UseMethod("map_data")
 
 #' @export 
 map_data.default <- function(map, ...) {
-  map_data("World")
+  map_data()
+}
+
+#' @export 
+#' @method map_data data.frame
+map_data.data.frame <- function(map, ...) {
+  return(map)
 }
 
 #' @export 
@@ -87,15 +211,15 @@ map_data.character <- function(map, ...) {
 
 #' @export 
 #' @method map_data SpatialPolygonsDataFrame
-map_data.SpatialPolygonsDataFrame <- function(map, ..., keep = 0.05) {
-  check_package("rmapshaper")
-  rmapshaper::ms_simplify(map, keep = keep)
+map_data.SpatialPolygonsDataFrame <- function(map, ...) {
+  check_package("geojsonio")
+  map <- geojsonio::geojson_list(map)
+  map_data(map)
 }
 
 #' @export 
 #' @method map_data geo_list
 map_data.geo_list <- function(map, ...) {
-  map <- geojsonio::geojson_list(map)
   class(map) <- "list"
   map_data(map)
 }
