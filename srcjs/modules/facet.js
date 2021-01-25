d14325d6252d7673f6080c3ea9fac087593d1e36
JohnCoene <jcoenep@gmail.com>
2021-01-25 22:02:27 +0100

    attempt at fixing planes for a custom figures, e.g.: fig_smooth, dig_density

--- srcjs/modules/facet.js
+++ srcjs/modules/facet.js
@@ -1,11 +1,20 @@
 import { gaugeFigure } from './gauges.js';
 import { addFigure } from './figures.js'; 
+import { getData } from './data.js'
 
 const facetFactory = (views) =>   {
   return function(view, facet){
     view.data(facet.data);
     
     views.forEach(function(v){
+
+      if(v.data){
+        facet.data = v.data.filter(function(row){
+          return row[facet.columnField] == facet.columnValue;
+        }) 
+      }
+
+      view.data(facet.data);
 
       let figure = addFigure(view, v.type);
       gaugeFigure(figure, v);
